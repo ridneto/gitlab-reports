34fba346b73db4814e3fb54439d87a48822260e9
ridneto <ridineu@hotmail.com>
2020-08-27 22:20:24 -0300

    criando entidade appointment

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,17 +1,15 @@
-import { uuid } from 'uuidv4';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity('appointments') // classe é uma entidade e qual a tabela
 class Appointment {
+  @PrimaryGeneratedColumn('uuid') // passo qual
   id: string;
 
+  @Column()
   provider: string;
 
+  @Column('timestamp with time zone')
   date: Date;
-
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    this.id = uuid();
-    this.provider = provider;
-    this.date = date;
-  }
 }
 
 export default Appointment;
