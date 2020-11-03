d9f2becf82c714d41ac51ce0c3c9e78ce7bc9b07
ridneto <ridineu@hotmail.com>
2020-11-03 01:03:34 -0300

    adc funcinalidade reset senha por email; pendente criar estrutura disparo email

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -44,7 +44,7 @@ describe('AuthenticateUser', () => {
       fakeHashProvider,
     );
 
-    expect(
+    await expect(
       authenticateUser.execute({
         email: 'johndoe@example.com',
         password: '123',
@@ -72,7 +72,7 @@ describe('AuthenticateUser', () => {
       password: '123',
     });
 
-    expect(
+    await expect(
       authenticateUser.execute({
         email: 'johndoe@example.com',
         password: 'wrong-password',
